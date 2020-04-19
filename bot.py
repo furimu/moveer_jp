520813a74fdb627878d5c01066314b3936f7b828
furimu <63914180+furimu@users.noreply.github.com>
2020-04-19 20:37:41 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,19 +53,7 @@ async def on_ready():
 
 @bot.event
 async def on_guild_join(guild):
-    mes = await guild.owner.send('このBOTは「本家」Moveerと使い方は同じです。しかし、コマンドを送信したら本家とこのBOTが反応してしまうため、下記のリアクションを押したら、本家の方を自動退出させます。**後々このBotが乗っ取られ、他のメンバーもキックされてしまうと思われる場合は、このBotについてる役職のメンバーをキックする権限をオフにする事をオススメします。** 同意する場合は、下記のリアクションを押してください。')
-    await mes.add_reaction('\N{OK HAND SIGN}')
-    def check(reaction, user):
-        return reaction.message.id == mes.id and user == guild.owner
-
-    reaction, user = await bot.wait_for('reaction_add', check = check)
-
-    if str(reaction.emoji) == '\N{OK HAND SIGN}':
-        user = bot.get_user(400724460203802624)
-        await guild.kick(user)
-
-        await guild.owner.send('Moveerをキックしました！')
-
+    mes = await guild.owner.send('このBOTは「本家」Moveerと使い方は同じです。しかし、コマンドを送信したら本家とこのBOTが反応してしまうため、本家様の方を手動で退出させてください。自動で本家様を退出させるのは権限の仕様上不可能となっています。')
 
 @bot.command()
 async def changema(ctx, channel: TextChannel = None):
